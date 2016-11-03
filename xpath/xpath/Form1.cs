b46da1dcac876d289e550b71e90e4872d245ff2e
Bill <billmcmillin@gmail.com>
2016-11-03 11:39:06 -0400

    Initial desktop app

---
+++ xpath/xpath/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace xpath
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String enteredValue = textBox1.Text;
+            enteredValue = "Your query: " + enteredValue;
+            label1.Text = enteredValue;
+        }
+    }
+}
